939818700f71aeec6671c36d01aa2cb2981e10ab
edersonschmeing <edersonschmeing@gmail.com>
2020-04-11 18:18:48 -0300

    curso linguagem c e cpp

--- C-II-Avancando-na-linguagem-jogo-forca/ponteiro.c
+++ C-II-Avancando-na-linguagem-jogo-forca/ponteiro.c
@@ -7,10 +7,22 @@ void calcula(int* c2) {
 	printf("calcula %d %d\n", (*c2), c2);
 }
 
+void soma(int* num, int a, int b) {
+    *num = a + b;
+}
+
+
 int main() {
 	int c = 10;
 
 	printf("main %d %d\n", c, &c);
 	calcula(&c);
 	printf("main %d %d\n", c, &c);
+
+
+	int num = 0;
+	soma(&num, 1, 3);
+
+	printf("soma %d\n", num);
+
 }
